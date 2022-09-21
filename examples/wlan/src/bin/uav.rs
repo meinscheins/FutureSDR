d71f07a5f7e008c6c93af272427fcb4809f3edb7
Bastian Bloessl <mail@bastibl.net>
2022-09-21 22:22:29 +0200

    wlan: uav cont

--- examples/wlan/src/bin/uav.rs
+++ examples/wlan/src/bin/uav.rs
@@ -108,6 +108,50 @@ fn main() -> Result<()> {
     let mut fg = Flowgraph::new();
 
     // ============================================
+    // TRANSMITTER
+    // ============================================
+    let mac = fg.add_block(Mac::new([0x42; 6], [0x23; 6], [0xff; 6]));
+    let encoder = fg.add_block(Encoder::new(args.mcs));
+    fg.connect_message(mac, "tx", encoder, "tx")?;
+    let mapper = fg.add_block(Mapper::new());
+    fg.connect_stream(encoder, "out", mapper, "in")?;
+    let mut fft = Fft::with_options(
+        64,
+        FftDirection::Inverse,
+        true,
+        Some((1.0f32 / 52.0).sqrt()),
+    );
+    fft.set_tag_propagation(Box::new(fft_tag_propagation));
+    let fft = fg.add_block(fft);
+    fg.connect_stream(mapper, "out", fft, "in")?;
+    let prefix = fg.add_block(Prefix::new(PAD_FRONT, PAD_TAIL));
+    fg.connect_stream_with_type(
+        fft,
+        "out",
+        prefix,
+        "in",
+        Circular::with_size(prefix_in_size),
+    )?;
+    let mut soapy = SoapySinkBuilder::new()
+        .freq(args.tx_channel)
+        .sample_rate(args.sample_rate)
+        .gain(args.tx_gain);
+    if let Some(a) = args.tx_antenna {
+        soapy = soapy.antenna(a);
+    }
+    if let Some(f) = args.tx_filter {
+        soapy = soapy.filter(f);
+    }
+    let soapy_snk = fg.add_block(soapy.build());
+    fg.connect_stream_with_type(
+        prefix,
+        "out",
+        soapy_snk,
+        "in",
+        Circular::with_size(prefix_out_size),
+    )?;
+
+    // ============================================
     // RECEIVER
     // ============================================
     let mut soapy = SoapySourceBuilder::new()
@@ -161,50 +205,6 @@ fn main() -> Result<()> {
     let (rxed_sender, mut rxed_frames) = mpsc::channel::<Pmt>(100);
     let message_pipe = fg.add_block(MessagePipe::new(rxed_sender));
     fg.connect_message(decoder, "rx_frames", message_pipe, "in")?;
-
-    // ============================================
-    // TRANSMITTER
-    // ============================================
-    let mac = fg.add_block(Mac::new([0x42; 6], [0x23; 6], [0xff; 6]));
-    let encoder = fg.add_block(Encoder::new(args.mcs));
-    fg.connect_message(mac, "tx", encoder, "tx")?;
-    let mapper = fg.add_block(Mapper::new());
-    fg.connect_stream(encoder, "out", mapper, "in")?;
-    let mut fft = Fft::with_options(
-        64,
-        FftDirection::Inverse,
-        true,
-        Some((1.0f32 / 52.0).sqrt()),
-    );
-    fft.set_tag_propagation(Box::new(fft_tag_propagation));
-    let fft = fg.add_block(fft);
-    fg.connect_stream(mapper, "out", fft, "in")?;
-    let prefix = fg.add_block(Prefix::new(PAD_FRONT, PAD_TAIL));
-    fg.connect_stream_with_type(
-        fft,
-        "out",
-        prefix,
-        "in",
-        Circular::with_size(prefix_in_size),
-    )?;
-    let mut soapy = SoapySinkBuilder::new()
-        .freq(args.tx_channel)
-        .sample_rate(args.sample_rate)
-        .gain(args.tx_gain);
-    if let Some(a) = args.tx_antenna {
-        soapy = soapy.antenna(a);
-    }
-    if let Some(f) = args.tx_filter {
-        soapy = soapy.filter(f);
-    }
-    let soapy_snk = fg.add_block(soapy.build());
-    fg.connect_stream_with_type(
-        prefix,
-        "out",
-        soapy_snk,
-        "in",
-        Circular::with_size(prefix_out_size),
-    )?;
 
     let rt = Runtime::new();
     let (fg, mut handle) = block_on(rt.start(fg));
@@ -297,6 +297,17 @@ fn main() -> Result<()> {
                     } else {
                         warn!("pmt to tx was not a blob");
                     }
+                } else {
+                    warn!("cannot read from MessagePipe receiver");
+                }
+            }
+        });
+    } else {
+        info!("No UDP forwarding configured");
+        rt.spawn_background(async move {
+            loop {
+                if let Some(_p) = rxed_frames.next().await {
+                    info!("FRAAAAAAAAME");
                 } else {
                     warn!("cannot read from MessagePipe receiver");
                 }
